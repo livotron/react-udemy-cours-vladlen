3443be29483de1e38456c33f2bd4f1e80c89fb3d
unknown <smirnoff.yevhen@gmail.com>
2020-11-11 15:50:42 +0200

    minor fix

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -2,6 +2,6 @@ export default (props) => (
     <div>
         <h3>Car Name: {props.name}</h3>
         <p>Year: {props.year}</p>
-        <button onClick={props.onChangeTitle}>Change Title</button>
+        <button onClick={props.onChangeTitle}>Change Title  </button>
     </div>
 )
